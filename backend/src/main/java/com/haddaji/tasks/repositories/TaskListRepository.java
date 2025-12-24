18292b0a6989f025b7460312822a23c243b02d21
Haddajii <mohamedhaddaji68@gmail.com>
2025-12-24 14:57:10 +0100

    add task and taskList repositories

---
+++ backend/src/main/java/com/haddaji/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,11 @@
+package com.haddaji.tasks.repositories;
+
+import com.haddaji.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList , UUID> {
+}
